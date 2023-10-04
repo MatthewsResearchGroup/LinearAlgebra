de4e26cfa2378561caf78af04849890477d8906b
Chao Yin <chaoyin1994@gmail.com>
2023-10-04 14:36:35 -0500

    Implement the unblockrightlooking.hpp.

--- ltlt/unblockedRightLooking.hpp
+++ ltlt/unblockedRightLooking.hpp
@@ -16,18 +16,31 @@
 using namespace MArray;
 using std::tie;
 
-void ltlt_unblockeRL(const row_view<const double>& lx0, const matrix_view<double>& X)
+void ltlt_unblockeRL(const matrix_view<double>& X, len_type k = -1, bool first_column = false)
 {
     auto [T, m, B] = partition_rows<DYNAMIC, 1 DYNAMIC>(X);
-
+    auto n = X.length(0);
+    if (k == -1)
+        k = n;
+    matrix_view<double> L = first_column ? X.shifted(1, -1) : X.rebased(1, 1);
     row<double> temp{X.length(0)};
+    if (first_column)
+        blas::skr2(1.0, L[B, m], X[B, m], 1.0, X[B, B]);
 
-    while{B}
+    while(B.size() > n - k)
     {
         // (T  || m  |   B    )
         // (R0 || r1 | r2 | R3) 4 * 4 partition
 
-        atuo [R0, r1, r2, R3] = repartition<DYNAMIC, 1>(T, m, B, 1)
+        auto [R0, r1, r2, R3] = repartition(T, m, B);
+        
+        L[R3,r2] = X[R3, r1] / X[r2, r1];
+        
+        blas::skr2(1.0, L[R3, r2], X[R3, r2], 1.0, X[R3, R3]);
+
+        // (R0 | r1 || r2 | R3 )
+        // (T       || m  | B  )
+        tie(T, m, B) = continue_with(R0, r1, r2, R3);
     }
 }
 
