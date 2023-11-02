c32a40fb50695d51166c9dd4eb0318b7fa4b98f4
Chao Yin <chaoyin1994@gmail.com>
2023-11-01 23:44:53 -0500

    add a test function for unblock algorithms

--- ltlt/ltlt.cpp
+++ ltlt/ltlt.cpp
@@ -1,34 +1,70 @@
 #include "ltlt.hpp"
 
-#include <random>
+template <class T>
+void print(T& X)
+{
+    auto n = X.length(0);
+    for (auto i : range(n))
+    {
+        for (auto j : range(n))
+        {
+            printf("%e,", X[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 int main()
 {
     /*
-     *  testing unblockLeftLooking first.
+     *  testing with different matrix size
      *
      */
+    // auto n = 5; // square matrix size
+    for (auto n = 10; n <= 100; n = n + 10)
+        test(n, ltlt_unblockLL);
 
-    // build a skew symmtri matrix first.
-    // B = A - A.T
+    // matrix<double> A{n, n};
 
-    auto n = 100; // square matrix size
+    // // initialize matrix A
+    // std::mt19937_64 rng;
+    // std::uniform_real_distribution<> uniform;
+    // A.for_each_element([&](auto& Aij){ Aij = uniform(rng); });
 
-    matrix<double> A{n, n};
+    // matrix<double> B = A - A.T();
+    // matrix<double> B_o = B;
+    // printf("B\n");
+    // print(B);
+    // printf("----------------------------\n");
+    // ltlt_blockRL(B, 4, ltlt_unblockRL);
+    // printf("B after ltlt\n");
+    // print(B);
+    // printf("----------------------------\n");
+    // auto M = make_L(B);
+    // printf("L\n");
+    // print(M);
+    // printf("----------------------------\n");
+    // auto MT = M.T(); 
+    // printf("L.T()\n");
+    // print(MT);
+    // printf("----------------------------\n");
+    // auto T = make_T(B);
+    // printf("T\n");
+    // print(T);
+    // printf("----------------------------\n");
+    // auto Y = MArray::blas::gemm(MArray::blas::gemm(M,T), MT);
+    // printf("Y\n");
+    // print(Y);
+    // printf("----------------------------\n");
 
-    // initialize matrix A
-    std::mt19937_64 rng;
-    std::uniform_real_distribution<> uniform;
-    A.for_each_element([&](auto& Aij){ Aij = uniform(rng); });
-
-    matrix<double> B = A - A.T();
-
-    for (auto i : range(n))
-        for (auto j : range(n))
-            printf("%f\n", B[i][j]);
+    // B_o -= Y;
+    // auto err = norm(B_o);
+    // MARRAY_ASSERT(err > 1e-12);
+    // // printf("The norm of error matrix: %e", err);
 
     return 0;
 
+    // 
     // timing
     // bli_clock.h   bli_clock
     // count the FLOPs of different algorithms
@@ -39,4 +75,17 @@ int main()
     // testing
     //
     // (A - L @ T @ L.T()) v
+    //
+
+
+    // 
+    //
+    // Poviting 
+    //
+    //
+
+    //
+    // two step right-looking algorithm
+    //
+    //
 }
--- ltlt/ltlt.hpp
+++ ltlt/ltlt.hpp
@@ -6,6 +6,7 @@
 #include <tuple>
 #include <array>
 #include <functional>
+#include <random>
 
 //must come first
 #define MARRAY_USE_BLIS
@@ -14,6 +15,7 @@
 #include "expression.hpp"
 #include "blas.h"
 #include "flame.hpp"
+#include "bli_clock.h"
 
 using namespace MArray;
 using MArray::slice::all;
@@ -115,6 +117,114 @@ template <typename T> range_t<T> not_first(const range_t<T>& x)
 {
     return range(x.from()+1, x.to());
 }
+
+inline matrix<double> make_L(const matrix_view<const double>& X)
+{
+    auto n = X.length(0);
+    matrix<double> B{n, n};
+
+    for (auto i : range(n))
+    for (auto j : range(n))
+    {
+        if (j < i && j > 0)
+            B[i][j] = X[i][j-1];
+        if (j == i)
+            B[i][j] = 1.0;
+    };
+    return B;
+}
+
+inline matrix<double> make_T(const matrix_view<const double>& X)
+{
+    auto n = X.length(0);
+    matrix<double> B{n, n};
+    for (auto i : range(n))
+    for (auto j : range(i))
+    {
+        if (i == j + 1)
+        {
+            B[i][j] = X[i][j];
+            B[j][i] = -X[i][j];
+        }
+    }
+    return B;
+}
+
+
+/**
+ * Return the squared 2-norm of the given tensor.
+ *
+ * @tparam Tensor   The type of the tensor, should be a tensor, view, or partially-indexed tensor.
+ *
+ * @param x     A tensor view.
+ *
+ * @return      The squared 2-norm, which is equal to the sum of squares of the elements.
+ */
+template <typename Tensor>
+inline double norm2(const Tensor& x)
+{
+    auto nrm = 0.0;
+    x.view().for_each_element([&](double e) { nrm += e*e; });
+    return nrm;
+}
+
+/**
+ * Return the 2-norm of the given tensor.
+ *
+ * @tparam Tensor   The type of the tensor, should be a tensor, view, or partially-indexed tensor.
+ *
+ * @param x     A vector or tensor view.
+ *
+ * @return      The 2-norm, which is equal to the square root of the sum of squares of the elements.
+ */
+template <typename Tensor>
+inline double norm(const Tensor& x)
+{
+    // Could suffer from overflow issues...
+    return sqrt(norm2(x));
+}
+
+// testing function
+inline void test(int n, const std::function<void(const matrix_view<double>&,len_type,bool)>& LTLT_UNB)
+{
+    // build the matrix
+    matrix <double> A{n, n};
+
+    // initialize matrix A
+    std::mt19937_64 rng;
+    std::uniform_real_distribution<> uniform;
+    A.for_each_element([&](auto& Aij){ Aij = uniform(rng); });
+
+    // make skew symmetric matrix
+    matrix<double> B = A - A.T();
+    
+    // make a copy of B since we need to overwrite part of B
+    matrix<double> B_deepcopy = B;
+    
+    // starting the decompostion
+    // recode the time
+    //
+    auto starting_point =  bli_clock();
+    LTLT_UNB(B, -1, false);
+    auto ending_point = bli_clock();
+
+    // calculate the time for decompostion
+    auto time = ending_point - starting_point;
+
+    // verify its correctness
+    // make L and T from B
+
+    auto Lm = make_L(B);
+    auto Tm = make_T(B);
+    auto LmT = Lm.T();
+
+    B_deepcopy -= MArray::blas::gemm(MArray::blas::gemm(Lm,Tm), LmT);
+    auto err = norm(B_deepcopy);
+    MARRAY_ASSERT(err > 1e-12);
+
+    printf("finish successfully\n");
+} 
+
 
 void ltlt_unblockRL(const matrix_view<double>& X, len_type k = -1, bool first_column = false);
 
